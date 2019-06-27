4c97d11772d2512a5a9fe0872bb46d5746c67edf
GilServer <gilm2121@gmail.com>
2019-06-27 19:39:20 +0000

    added a crontab to update youtube-dl and change youtube-dl update script to use pip

--- update-youtubedl.sh
+++ update-youtubedl.sh
@@ -1,8 +1,2 @@
-mkdir temp_youtubedl_update
-cd temp_youtubedl_update
-sudo apt-get remove -y youtube-dl
-sudo wget https://yt-dl.org/latest/youtube-dl -O /usr/local/bin/youtube-dl
-sudo chmod a+x /usr/local/bin/youtube-dl
-hash -r
-cd ..
-rm -r temp_youtubedl_update
+pip install --upgrade youtube-dl
+
